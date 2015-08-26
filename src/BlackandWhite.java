27420ae684df1f9e1d5aa95ada062810cc255a85
Udit1111 <u2pancha@uwaterloo.ca>
2015-08-26 19:58:30 -0400

    First Part Done

--- src/BlackandWhite.java
+++ src/BlackandWhite.java
@@ -1,14 +1,31 @@
+import java.io.*;
+import java.util.Scanner;
 
 public class BlackandWhite {
 	
+	static int rotationCounter = 0;
+	
 	public static void main(String[] args)
 	{	
-		System.out.println();
-		
-		int udit; 
-		String Urvi;
-		
-		Hello world;
-		Again Work Done;
+		Scanner scan = new Scanner(System.in);
+		rotationCounter = Integer.parseInt(scan.nextLine());
+		long[] results = calculateprice(9, 1, 1);
+		System.out.println(results[0]+""+results[1]);}
+	
+	public static long[] calculateprice( long blackPrice, long whitePrice, long interchangePrice)
+	{
+		long[] returnValue = new long[2];
+		if(blackPrice < interchangePrice && whitePrice < interchangePrice){
+			returnValue[0] = blackPrice;
+			returnValue[1] = whitePrice;}
+		else if(blackPrice > interchangePrice && blackPrice > (interchangePrice + whitePrice)){
+			returnValue[0] = interchangePrice + whitePrice;
+			returnValue[1] = whitePrice;	
+		}
+		else if(whitePrice > interchangePrice && whitePrice > (interchangePrice + blackPrice)){
+			returnValue[0] = blackPrice;
+			returnValue[1] = interchangePrice + blackPrice;	
+		}
+		return returnValue;
 	}
 }
